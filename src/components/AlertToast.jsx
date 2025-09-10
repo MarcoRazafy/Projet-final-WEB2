87fad37e01bc020d6adbe91eb69309bbdba3375f
Mathieu Harison <hei.mathieu.95@gmail.com>
2025-09-10 12:21:17 +0300

    changement globale

--- src/components/AlertToast.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const AlertToast = ({ message }) => (
-  <div className="p-4 mb-4 bg-red-100 text-red-800 rounded shadow">
-    {message}
-  </div>
-);
-
-export default AlertToast;
